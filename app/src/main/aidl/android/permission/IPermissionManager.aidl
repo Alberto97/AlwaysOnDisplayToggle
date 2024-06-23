34c1584ee8f8e88b71b3a4c88aec2d4a610eb7ca
Alberto Pedron <albertop2197@gmail.com>
2024-06-24 00:51:35 +0200

    Properly support Android 14 and replace AIDL with stubs
    AIDL is nice but does not support overloading :(

--- app/src/main/aidl/android/permission/IPermissionManager.aidl
+++
@@ -1,5 +0,0 @@
-package android.permission;
-
-interface IPermissionManager {
-    void grantRuntimePermission(String packageName, String permissionName, int userId);
-}
--- app/src/main/java/org/alberto97/aodtoggle/permissions/PlatformPermissionManager.kt
+++ app/src/main/java/org/alberto97/aodtoggle/permissions/PlatformPermissionManager.kt
@@ -1,5 +1,7 @@
 package org.alberto97.aodtoggle.permissions
 
+import android.annotation.TargetApi
+import android.content.Context
 import android.content.pm.IPackageManager
 import android.os.Build
 import android.permission.IPermissionManager
@@ -14,23 +16,79 @@ class PlatformPermissionManager {
         const val PERMISSION_MANAGER_SERVICE = "permissionmgr"
     }
 
-    private val packageManager = IPackageManager.Stub.asInterface(
-        ShizukuBinderWrapper(SystemServiceHelper.getSystemService(PACKAGE_MANAGER_SERVICE))
-    )
+    fun grantRuntimePermission(packageName: String, permissionName: String, userId: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+            return Api34Impl.grantRuntimePermission(packageName, permissionName, userId)
 
-    private val permissionManager: IPermissionManager by lazy {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            HiddenApiBypass.addHiddenApiExemptions("Landroid/permission")
-        }
-        IPermissionManager.Stub.asInterface(
-            ShizukuBinderWrapper(SystemServiceHelper.getSystemService(PERMISSION_MANAGER_SERVICE))
-        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+            return Api30Impl.grantRuntimePermission(packageName, permissionName, userId)
+
+        return LegacyImpl.grantRuntimePermission(packageName, permissionName, userId)
     }
 
-    fun grantRuntimePermission(packageName: String?, permissionName: String?, userId: Int) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+    @TargetApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    object Api34Impl {
+        private const val PERSISTENT_DEVICE_ID_DEFAULT = "default:" + Context.DEVICE_ID_DEFAULT
+
+        private val permissionManager: IPermissionManager by lazy {
+            HiddenApiBypass.addHiddenApiExemptions("Landroid/permission")
+            IPermissionManager.Stub.asInterface(
+                ShizukuBinderWrapper(SystemServiceHelper.getSystemService(PERMISSION_MANAGER_SERVICE))
+            )
+        }
+
+        fun grantRuntimePermission(packageName: String, permissionName: String, userId: Int) {
+            try {
+                // Android 14 QPR3+
+                permissionManager.grantRuntimePermission(
+                    packageName,
+                    permissionName,
+                    PERSISTENT_DEVICE_ID_DEFAULT,
+                    userId
+                )
+                return
+            } catch (e: NoSuchMethodError) {
+                // No-op
+            }
+
+            try {
+                // Android 14 QPR2
+                permissionManager.grantRuntimePermission(
+                    packageName,
+                    permissionName,
+                    Context.DEVICE_ID_DEFAULT,
+                    userId
+                )
+                return
+            } catch (e2: NoSuchMethodError) {
+                // No-op
+            }
+
+            // Android 14 initial release
             permissionManager.grantRuntimePermission(packageName, permissionName, userId)
-        } else {
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.R)
+    object Api30Impl {
+        private val permissionManager: IPermissionManager by lazy {
+            HiddenApiBypass.addHiddenApiExemptions("Landroid/permission")
+            IPermissionManager.Stub.asInterface(
+                ShizukuBinderWrapper(SystemServiceHelper.getSystemService(PERMISSION_MANAGER_SERVICE))
+            )
+        }
+
+        fun grantRuntimePermission(packageName: String, permissionName: String, userId: Int) {
+            permissionManager.grantRuntimePermission(packageName, permissionName, userId)
+        }
+    }
+
+    object LegacyImpl {
+        private val packageManager = IPackageManager.Stub.asInterface(
+            ShizukuBinderWrapper(SystemServiceHelper.getSystemService(PACKAGE_MANAGER_SERVICE))
+        )
+
+        fun grantRuntimePermission(packageName: String, permissionName: String, userId: Int) {
             packageManager.grantRuntimePermission(packageName, permissionName, userId)
         }
     }
